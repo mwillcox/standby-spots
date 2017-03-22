d2b8c5e6e4339b11c044fea1557e8a213019bf40
Michelle Willcox <shellwillcox@gmail.com>
2017-03-21 21:00:39 -0700

    fixed type column error in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,23 @@
 require 'csv'
 
-
-csv_text = File.read(Rails.root.join('db', 'seeds', 'parklets.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-csv.each do |row|
+parklets_csv_text = File.read(Rails.root.join('db', 'seeds', 'parklets.csv'))
+parklets_csv = CSV.parse(parklets_csv_text, :headers => true, :encoding => 'ISO-8859-1')
+parklets_csv.each do |row|
   s = Spot.new
   s.name = row['envista_project_name_full']
-  s.type = 'Parklet'
+  s.park_type = 'Parklet'
   s.address = row['envista_intermediate_location_text']
   s.location = row['Location']
   s.save
 end
 puts "There are now #{Spot.count} rows in the transactions table"
 
-csv_text2 = File.read(Rails.root.join('db', 'seeds', 'popos.csv'))
-csv2 = CSV.parse(csv_text2, :headers => true, :encoding => 'ISO-8859-1')
-csv2.each do |row|
+popos_csv_text = File.read(Rails.root.join('db', 'seeds', 'popos.csv'))
+popos_csv = CSV.parse(popos_csv_text, :headers => true, :encoding => 'ISO-8859-1')
+popos_csv.each do |row|
   s = Spot.new
   s.name = row['NAME']
-  s.type = 'Privately Owned Public Open Space'
+  s.park_type = 'Privately Owned Public Open Space'
   s.description = row['Descriptio']
   s.address = row['POPOS_ADDR']
   s.location = row['the_geom']
