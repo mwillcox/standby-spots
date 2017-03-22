86ca1c8bfc9d0b03e00028973513d341066e33bf
Michelle Willcox <shellwillcox@gmail.com>
2017-03-21 20:51:56 -0700

    uncommenting seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,27 @@
 require 'csv'
 
 
-# csv_text = File.read(Rails.root.join('db', 'seeds', 'parklets.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-#   s = Spot.new
-#   s.name = row['envista_project_name_full']
-#   s.type = 'Parklet'
-#   s.address = row['envista_intermediate_location_text']
-#   s.location = row['Location']
-#   s.save
-# end
-# puts "There are now #{Spot.count} rows in the transactions table"
+csv_text = File.read(Rails.root.join('db', 'seeds', 'parklets.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  s = Spot.new
+  s.name = row['envista_project_name_full']
+  s.type = 'Parklet'
+  s.address = row['envista_intermediate_location_text']
+  s.location = row['Location']
+  s.save
+end
+puts "There are now #{Spot.count} rows in the transactions table"
 
-# csv_text = File.read(Rails.root.join('db', 'seeds', 'popos.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-#   s = Spot.new
-#   s.name = row['NAME']
-#   s.type = 'Privately Owned Public Open Space'
-#   s.description = row['Descriptio']
-#   s.address = row['POPOS_ADDR']
-#   s.location = row['the_geom']
-#   s.save
-# end
-# puts "There are now #{Spot.count} rows in the transactions table"
+csv_text = File.read(Rails.root.join('db', 'seeds', 'popos.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  s = Spot.new
+  s.name = row['NAME']
+  s.type = 'Privately Owned Public Open Space'
+  s.description = row['Descriptio']
+  s.address = row['POPOS_ADDR']
+  s.location = row['the_geom']
+  s.save
+end
+puts "There are now #{Spot.count} rows in the transactions table"
